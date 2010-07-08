1a19010948f93099e1150f3fc1bb72d653b4e0d9
Adam Lloyd <adam@alloy-d.net>
2010-07-08 13:26:52 -0400

    Implement `now -done`.

--- tools/now.go
+++ tools/now.go
@@ -1,11 +1,19 @@
 package main
 
 import (
+    "flag"
     "fmt"
     "../now"
 )
 
+var done *bool = flag.Bool("done", false, "Current thing is finished")
+
 func main() {
+    flag.Parse()
+    if *done {
+        now.Pop()
+    }
+
     thing := now.GetNext()
 
     fmt.Printf("%s\n", thing)
