55c8b4e78566c1f6bef73ea4edbe6049eae4cfcb
Adam Lloyd <adam@alloy-d.net>
2010-07-08 11:53:31 -0400

    Add/improve JSON handlers.

--- serve-to-machines.go
+++ serve-to-machines.go
@@ -7,10 +7,14 @@ import (
 )
 
 func jsonNext(ctx *web.Context) string {
-    out, _ := json.Marshal(GetNext())
+    if thing := GetNext(); thing != "" {
+        out, _ := json.Marshal(thing)
 
-    ctx.SetHeader("Content-Type", "application/json", true)
-    return fmt.Sprintf("%s", out)
+        ctx.SetHeader("Content-Type", "application/json", true)
+        return fmt.Sprintf("%s", out)
+    }
+    ctx.StartResponse(418)  // I'm a teapot
+    return ""
 }
 
 func jsonLater(ctx *web.Context) {
@@ -24,3 +28,24 @@ func jsonLater(ctx *web.Context) {
     }
 }
 
+func jsonPop(ctx *web.Context) string {
+    if _, ok := ctx.Request.Params["done"]; ok {
+        Pop()
+        return jsonNext(ctx)
+    }
+
+    ctx.StartResponse(400)
+    return ""
+}
+
+func jsonPushNext(ctx *web.Context) string {
+    if thing, ok := ctx.Request.Params["thing"]; ok {
+        if len(thing) > 1 {
+            ctx.StartResponse(400)
+            return ""
+        }
+
+        PushNext(thing[0])
+    }
+    return jsonNext(ctx)
+}
