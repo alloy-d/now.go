1ebfc4dfeb39cb37e63d4326ba7ddf80ea7057d5
Adam Lloyd <adam@alloy-d.net>
2010-07-11 04:25:10 -0400

    Oops, left an extra hack in the dirty rotten hack.

--- dirty-rotten-hacks.go
+++ dirty-rotten-hacks.go
@@ -1,7 +1,6 @@
 package now
 
 import (
-    "fmt"
     "path"
     "runtime"
 )
@@ -9,6 +8,5 @@ import (
 func ResourceDir() string {
     _, file, _, _ := runtime.Caller(0)
     dir, _ := path.Split(file)
-    fmt.Println(dir)
     return path.Join(dir, "human-friendly")
 }
