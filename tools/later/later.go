b746311c626fe1f9bda80d3069f3d72a80db2450
Adam Lloyd <adam@alloy-d.net>
2010-07-22 22:23:50 -0400

    Pushing to front belongs in later.

--- tools/later/later.go
+++ tools/later/later.go
@@ -9,9 +9,15 @@ import (
 )
 
 var addr *string = flag.String("http", ":5939", "HTTP service address")
+var first *bool = flag.Bool("first", false, "Thing should be done first")
 
 func addThing(thing string) {
-    _, err := http.Post("http://" + *addr + "/later/", "text/plain", strings.NewReader("thing=" + thing))
+    path := "/later/"
+    if *first {
+        path = "/now"
+    }
+
+    _, err := http.Post("http://" + *addr + path, "text/plain", strings.NewReader("thing=" + thing))
     if err != nil {
         fmt.Fprintf(os.Stderr, "http.Post: %v", err)
         os.Exit(1)
